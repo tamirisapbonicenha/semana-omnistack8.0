ba1b41fd69d8e1e1390d79e0f9dee5e14e593d4e
Tamiris Bonicenha <tamirisapbonicenha@gmail.com>
2019-08-16 01:23:23 -0300

    finish web app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,7 +7,9 @@ const routes = require('./routes');
 
 const server = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-b5t7z.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+console.log(process.env.DB_USER)
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-b5t7z.mongodb.net/omnistack8?retryWrites=true&w=majority`, {
   useNewUrlParser: true
 });
 
